2768992c2f2c2ad0b0f631f473e7525253271ff8
Samuel <samueljonascali@gmail.com>
2025-01-01 15:08:35 -0300

    Update gemfile to add will paginate for bootstrap, modify posts_controller

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem "jbuilder"
 gem 'devise'
 #Gem for paginate
 gem 'will_paginate', '~> 3.3'
+gem 'bootstrap-will_paginate'
+
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,46 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.order(create_at: :desc).paginate(page: params[:page], per_page: 3)
   end
 
   def show
+    @comment = Comment.new
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "Post created successfully"
+    else
+      render :new
+    end
   end
 
-  def edit
+  def edit;
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post updated successfully"
+    else
+      render :edit
+
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to post_path, notice: 'Post exclude successfully'
+  end
+  def set_post
+    @post = Post.find(params[:id])
+  end
+  
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
