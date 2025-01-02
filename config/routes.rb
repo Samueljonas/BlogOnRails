35b51a74c7aaf169be6d254fb66ef8a3b687f470
Samuel <samueljonascali@gmail.com>
2025-01-02 12:23:06 -0300

    Routes.rb are simplify now and add config.ru

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,19 @@
 Rails.application.routes.draw do
-  get "comments/create"
-  get "comments/destroy"
-  get "posts/index"
-  get "posts/show"
-  get "posts/new"
-  get "posts/create"
-  get "posts/edit"
-  get "posts/update"
-  get "posts/destroy"
-  get "users/index"
-  get "users/show"
-  get "users/edit"
-  get "users/update"
-  get "users/destroy"
+  # Rotas para autenticação de usuários
   devise_for :users
+  resources :posts
 
-  # Routes for CRUD users
+  # Rotas para gerenciamento de usuários
   resources :users, only: [:index, :show, :edit, :update, :destroy]
 
-  # home page users
- # root to: 'users#index'
- #  root "pages#home"
-
-  #incluir rotas aninhadas de comentários em posts
+  # Página inicial (root)
   root to: "posts#index"
-  
+
+  # Rotas para posts com comentários aninhados
   resources :posts do
-    resources :comments, only: %i[create destroy]
+    resources :comments, only: [:create, :destroy]
   end
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Health check (opcional)
   get "up" => "rails/health#show", as: :rails_health_check
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
