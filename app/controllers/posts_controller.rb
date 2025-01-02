c9ca74ce886b4c0f381fa886c238604ad114a9a4
Samuel <samueljonascali@gmail.com>
2025-01-02 12:18:09 -0300

    update in posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,47 +1,62 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
+
   def index
     @posts = Post.order(created_at: :desc).paginate(page: params[:page], per_page: 3)
   end
 
   def show
+    @comments = @post.Comments # Certifique-se de que o modelo Post tem has_many :comments
     @comment = Comment.new
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(post_params)
+    puts "Requisição chegou no método CREATE com parâmetros: #{post_params.inspect}"
+    @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to @post, notice: "Post created successfully"
+      redirect_to @post, notice: "Post criado com sucesso!"
     else
-      render :new
+      flash.now[:alert] = "Erro ao criar o post: #{@post.errors.full_messages.join(', ')}"
+      render :new, status: :unprocessable_entity
     end
   end
+  
+  
 
-  def edit;
-  end
+  def edit; end
 
   def update
     if @post.update(post_params)
       redirect_to @post, notice: "Post updated successfully"
     else
-      render :edit
-
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @post.destroy
-    redirect_to post_path, notice: 'Post exclude successfully'
+    redirect_to posts_path, notice: 'Post deleted successfully'
   end
+
+  private
+
   def set_post
     @post = Post.find(params[:id])
   end
-  
+
+  def authorize_user!
+    unless @post.user == current_user
+      redirect_to posts_path, alert: "You don't have permission to perform this action"
+    end
+  end
+
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :description, :author)
   end
 end
