6829d788b554d06d99de35be43252fb3dbe1fd1a
Samuel <samueljonascali@gmail.com>
2025-01-02 12:20:24 -0300

    new migration and update on schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_01_172536) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_02_141215) do
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.string "author"
@@ -27,6 +27,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_01_172536) do
     t.text "description"
     t.string "author"
     t.text "body"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +46,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_01_172536) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "users"
 end
