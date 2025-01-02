281e724823ffefa7dcea8ca5e1c0e2a819b19dce
Samuel <samueljonascali@gmail.com>
2025-01-02 12:19:36 -0300

    Bug fix on models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :author, :body, :presence: true
+  validates :author, :body, presence: true
   belongs_to :post
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
-  validates :title, :author, :body, presence:true
-  has_many:Comments
+  belongs_to :user
+  validates :title, :author, :body, presence:true, length:{maximum: 100}
+  validates :body, presence:true
+  has_many:Comments, dependent: :destroy
+
 end
