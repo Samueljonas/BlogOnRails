461dc54574903409be7fe01dc787ade5fa43b327
Samuel <samueljonascali@gmail.com>
2025-01-02 09:46:42 -0300

    Update front-end

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
     @posts = Post.order(created_at: :desc).paginate(page: params[:page], per_page: 3)
   end
