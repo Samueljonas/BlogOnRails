9af340dfa238f471faaeda5abb1993de9a7585d7
Samuel <samueljonascali@gmail.com>
2025-01-02 22:25:07 -0300

    update "set_user"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,22 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user! #verify if user are authentic
-  before_action :set__user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user! # Garante que o usuário está autenticado
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :authorize_admin, except: [:index, :show]
 
+  # Lista todos os usuários (apenas para admins)
   def index
     @users = User.all
   end
 
+  # Mostra um único usuário
   def show
   end
 
+  # Exibe o formulário de edição do usuário
   def edit
   end
 
+  # Atualiza os dados do usuário
   def update
     if @user.update(user_params)
       redirect_to @user, notice: "User updated!"
@@ -21,21 +25,28 @@ class UsersController < ApplicationController
     end
   end
 
-
+  # Exclui um usuário
   def destroy
-    user.destroy
-  redirect_to users_path, notice: 'User exclude!'
+    @user.destroy
+    redirect_to users_path, notice: 'User excluded!'
   end
+
   private
 
-  def set__user
-    @user = User.find(params[:id])
+  # Define o usuário com base no ID passado na URL
+  def set_user
+    if params[:id].present? && params[:id] =~ /^\d+$/
+      @user = User.find(params[:id])
+    end
   end
+  
 
+  # Permite apenas os parâmetros especificados para segurança
   def user_params
-    params.require(:user).permit(:email,:password, :password_confirmation, :name)
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
 
+  # Apenas administradores podem acessar certas ações
   def authorize_admin
     redirect_to root_path, alert: "Unauthorized access!" unless current_user.admin?
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :author, :body, presence: true
   belongs_to :post
-
+  belongs_to :user, optional: true # Permite que o `user_id` seja nulo
+  validates :body, presence: true # Garante que o corpo do comentário seja preenchido
 end
