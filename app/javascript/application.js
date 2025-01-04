0b69596e0891810275caa51a4030da2e46571924
Samuel <samueljonascali@gmail.com>
2025-01-04 18:18:58 -0300

    Update bootstrap error

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,5 +2,6 @@
 import "@hotwired/turbo-rails";
 import "./controllers";
 import "bootstrap/dist/css/bootstrap.min.css"; // Importar CSS
+import "./stylesheets/custom.css";
 import Rails from "@rails/ujs";
 Rails.start();
