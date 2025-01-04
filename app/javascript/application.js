784ad9a51d77d8ab0530068fcbc86f32524c2040
Samuel <samueljonascali@gmail.com>
2025-01-04 17:10:58 -0300

    bug fix on bootstrap import

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 import "./controllers";
-import * as bootstrap from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css"; // Importar CSS
 import Rails from "@rails/ujs";
 Rails.start();
