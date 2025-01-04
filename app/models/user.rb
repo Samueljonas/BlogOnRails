59cc816c89425904ded992ef64632e186bb4396d
Samuel <samueljonascali@gmail.com>
2025-01-04 16:01:56 -0300

    Bug fix on "delete account"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :name, presence: true, length:{maximum: 50}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
