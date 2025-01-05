7ffef08f0a4ba79613c13bb93ad14f950eff0ea1
Samuel <samueljonascali@gmail.com>
2025-01-04 23:46:58 -0300

    timeout time increased

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,8 @@ Rails.application.configure do
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true,
+    open_timeout: 30, # Tempo de abertura da conexão
+    read_timeout: 60  # Tempo de leitura da resposta
   }
 
 
