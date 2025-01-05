959b9d38f8ee0ed635c6d9bb11c54fe5a90959e6
Samuel <samueljonascali@gmail.com>
2025-01-05 00:20:57 -0300

    try tls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,13 +62,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 465,
+    port:                 587,
     domain:               'gmail.com',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true,
-  
+    tls: true
   }
 
 
