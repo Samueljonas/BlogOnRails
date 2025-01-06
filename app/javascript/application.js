bd31c638723a1327205765da50adeef744f54a68
Samuel <samueljonascali@gmail.com>
2025-01-06 14:28:14 -0300

    clean code on application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 import "./controllers";
-import "bootstrap/dist/css/bootstrap.min.css"; // Importar CSS
-import "bootstrap"; // Primeiro o Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 import Rails from "@rails/ujs";
 Rails.start();
