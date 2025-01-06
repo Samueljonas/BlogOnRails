036cecc6b8da32d36f8f4b46f9024b76f8ae28f9
Samuel <samueljonascali@gmail.com>
2025-01-06 05:01:16 -0300

    db migrations: add user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_03_153917) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_06_075848) do
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.string "author"
@@ -29,6 +29,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_03_153917) do
     t.text "description"
     t.string "author"
     t.text "body"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
